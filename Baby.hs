44295248d33cb8a290f98d42e04f242e1a3d1feb
J Espinosa <jlikesmath@protonmail.com>
2019-06-15 14:42:46 -0700

    initial commit

---
+++ Baby.hs
@@ -0,0 +1,73 @@
+module Baby
+ (doubleSmallNumber
+ , doubleSmallNumberSucc
+ , boomBangs
+ , removeNonUppercase
+ , rightTriangles
+ , rightTriPer24
+ , jquickSort
+ , factorial
+ , circumference
+ , lucky
+ , sayMe
+ , addVectors
+ , first
+ , second
+ , third
+) where
+
+doubleSmallNumber x     = if x > 100
+						  then x 
+						  else x*2
+
+doubleSmallNumberSucc x = (if x > 100 then x else x*2) + 1
+
+boomBangs xs            = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]   
+
+removeNonUppercase st   = [c | c <- st, c `elem` ['A'..'Z'] ]
+
+rightTriangles          = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10], a^2+ b^2 ==c^2 ]
+
+rightTriPer24           = [ (a,b,c) | c <- [1..10], b <- [1.110], a <- [1..10], 
+                                       a^2 + b^2 == c^2, a+b+c==24 ]
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z  
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+jquickSort :: Ord a => [a] -> [a]
+-- Using list comprehensions
+jquickSort []     = []                               -- The empty list is already sorted
+jquickSort (x:xs) = jquickSort [a | a <- xs, a < x]   -- Sort the left part of the list
+                   ++ [x] ++                        -- Insert pivot between two sorted parts
+                   jquickSort [a | a <- xs, a >= x]  -- Sort the right part of the list
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKYNUMBERSEVEN!"
+lucky x = "fuck seven anyways"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "Uno!"
+sayMe 2 = "Dos!"
+sayMe 3 = "Tres!"
+sayMe 4 = "Cuatro!"
+sayMe 5 = "Cinco!"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a 
+factorial 0 = 1
+factorial n = n * factorial (n -1) 
+
+addVectors :: (Num a) => (a,a) -> (a,a) -> (a,a) 
+addVectors (x1, y1) (x2, y2) = (x1+ x2, y1 + y2) 
+
+first :: (a,b,c) -> a 
+first(x, _, _) = x
+ 
+second :: (a,b,c) -> b
+second (_, y, _) = y
+
+third :: (a,b,c,) ->
+third (_, _, z) = z 
---
+++ babySpec.hs
@@ -0,0 +1,80 @@
+module BabySpec where
+ 
+import Test.Hspec
+import Baby
+
+main :: IO ()
+main = hspec $ do 
+    describe "doubleSmallNumber" $ do
+      it "Doubles a number that is smaller than 100."  $
+        doubleSmallNumber 13 `shouldBe` 26
+      
+      it "leaves numbers larger than 100 as is" $
+        doubleSmallNumber 244 `shouldBe` 244
+        
+      it "doubles 100 to 200" $
+          doubleSmallNumber 100 `shouldBe` 200 
+
+    describe "doubleSmallNumberSucc" $ do 
+      it "doubles numbers less than 100 and than adds 1" $
+          doubleSmallNumberSucc 13 `shouldBe` 27
+      it "only increments numbers larger than 100 by one." $ 
+        doubleSmallNumberSucc 244 `shouldBe` 245
+      it "works on floats less than 100"  $
+        doubleSmallNumber 0.1 `shouldBe` 0.2
+      it "works on floats greater than 100" $
+        doubleSmallNumberSucc 101.010 `shouldBe` 102.010 
+
+    describe "lucky" $ do 
+        it "RETURNS STRING 'LUCKY NUMBER SEVEN!' if called with 7" $
+            lucky 7 `shouldBe` "LUCKYNUMBERSEVEN!"
+        it "gives a string saying 'fuck seven anyways' if called with not 7" $
+            lucky 22 `shouldBe` "fuck seven anyways" 
+        it "should throw a nice little type error if you feed it a Float" $
+            pendingWith "learning how errors work"
+
+    describe "jQuickSort" $ do 
+        it "returns an empty list if fed an empty list" $ 
+            jquickSort [1,2,3] `shouldBe` [1,2,3]
+        it  "returns [1,2,4,4,5] when given [5,4,1,4,2]" $
+            jquickSort [5,4,1,4,2] `shouldBe` [1,2,4,4,5]
+        it "sorts 'prof' into 'fopr'" $
+           jquickSort "prof" `shouldBe` "fopr"
+           
+    describe "sayMe" $ do
+        it "transmogrifies 1 to \"Uno!\"" $
+            sayMe 1 `shouldBe` "Uno!" 
+        it "transogrifies 1 to \"Dos!\"" $
+            sayMe 2 `shouldBe` "Dos!" 
+        it "transmogrifies 3 to \"Tres!\"" $
+            sayMe 3 `shouldBe` "Tres!" 
+        it "transmogrifies 4 to \"Cuatro!\"" $
+            sayMe 4 `shouldBe` "Cuatro!" 
+        it "transmogrifies 5 to \"Cinco!\"" $
+		    sayMe 5 `shouldBe` "Cinco!"
+        it "transmogrifies any other number to a standard message" $
+            sayMe (-3) `shouldBe` "Not between 1 and 5" 
+     
+    describe "factorial" $ do 
+        it "factorial of zero is 1" $ 
+            factorial 0 `shouldBe` 1
+        it "returns 479001600 for the factorial of 12" $
+            factorial 12 `shouldBe` 479001600
+        it "should throw some sort of error if fed a negative number" $
+            pendingWith "don't know how to quite handle that in haskell or hspec yet" 
+        
+    describe "addVectors" $ do 
+        it "works with a pair of int two-tuples" $
+            (2,3) `addVectors` (3,4) `shouldBe` (5,7)
+        it "works with a pair of float two-tuples" $  
+            (2.5, 3.55) `addVectors` (3.14, 6.28) `shouldBe` (5.64, 9.83)
+
+    describe "first" $ do 
+        it "grabs 'a' from ('a','b', 3)" $
+            first ('a','b', 3) `shouldBe` 'a'
+    describe "second" $ do 
+        it "grabs 'b' from ('a','b', .)" $ 
+            second ('a', 'b', 3) `shouldBe` 'b'
+    describe "third" $ do 
+        it "grabs 3 from ('a','b', 3)" $ 
+            third ('a','b', 3) `shouldBe 3
