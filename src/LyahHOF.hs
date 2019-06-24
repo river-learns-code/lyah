4ac227152e0bb1c248e6402343a75da6f8d66bae
J Espinosa <jlikesmath@protonmail.com>
2019-06-24 13:21:37 -0700

    add multwitheighteen and compareWithHundred for HOF chapter

--- src/LyahHOF.hs
+++ src/LyahHOF.hs
@@ -1,9 +1,16 @@
 module LyahHOF (
  multThree
- ,multTwoWithNine) 
+ ,multTwoWithNine
+ ,multWithEighteen
+ ,compareWithHundred) 
 where
 
 multThree :: (Num a) => a -> a -> a -> a 
 multThree x y z = x * y * z 
 
 multTwoWithNine  = multThree 9  
+
+multWithEighteen = multTwoWithNine 2
+
+compareWithHundred :: (Ord a, Num a) => a -> Ordering
+compareWithHundred  = compare 100 
--- test/LyahHOFSpec.hs
+++ test/LyahHOFSpec.hs
@@ -16,9 +16,12 @@ spec = do
     describe "multTwoWithNine" $ do
         it "multiplies two numbers together and multipilies with 9" $
 				multTwoWithNine 2 3 `shouldBe` 54
-    
-        
-        
+    describe "multWithEighteen" $ do 
+        it "multiplies a number with 18" $
+            multWithEighteen 10 `shouldBe` 180
+    describe "compareWithHundred" $ do 
+        it "compares with 100 and returns an ordering" $
+            compareWithHundred 35 `shouldBe` GT
         
         
         
