77319ef45fbe4000a22ff21d4d3063bda7e40ed7
J Espinosa <jlikesmath@protonmail.com>
2019-06-26 14:48:04 -0700

    finish HOF chapter! (update oddSquareSum)

--- src/LyahHOF.hs
+++ src/LyahHOF.hs
@@ -80,4 +80,7 @@ sqrtSums :: Int
 sqrtSums = length (takeWhile (<1000)  (scanl1 (+) (map sqrt [1..]))) +1
 
 oddSquareSum :: Integer
-oddSquareSum = sum . takeWhile (<10000) . filter odd . map (^2) $ [1..]
+oddSquareSum = 
+    let oddSquares = filter odd $ map (^2) [1..] 
+        belowLimit = takeWhile(<10000) oddSquares
+    in sum belowLimit
