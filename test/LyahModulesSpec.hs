cd4891148b37c6531e9874626b3f7d67f071e3cc
J Espinosa <jlikesmath@protonmail.com>
2019-06-27 16:00:55 -0700

    tests for transpose (Modules)

--- test/LyahModulesSpec.hs
+++ test/LyahModulesSpec.hs
@@ -22,5 +22,10 @@ spec = do
             intercalate [0,0,0] [[1,2,3],[4,5,6],[7,8,9]] `shouldBe` [1,2,3,0,0,0,4,5,6,0,0,0,7,8,9] 
         it "takes an empty list and just flattens" $
             intercalate "" ["fuck", "the","author","of","lyah"] `shouldBe` "fucktheauthoroflyah"
-        
-        
+    describe "transpose" $ do 
+        it "transposes lists oflists as if they were matrices" $ 
+            transpose [[1,2,3],[4,5,6],[7,8,9]] `shouldBe` [[1,4,7],[2,5,8],[3,6,9]]
+        it "transposes non rectangular 'matrices' too" $
+            transpose ["hey","there","folx"] `shouldBe` ["htf", "eho", "yel", "rx", "e"] 
+        it "can be used to model polynomial addition" $ 
+            map sum ( transpose [[0,3,5,9],[10,0,0,9],[8,5,1,-1]])  `shouldBe` [18,8,6,17]
